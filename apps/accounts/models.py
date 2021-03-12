714e59f6243ea917c1e586a1cabe4c8edc435aad
Max Garin <maxgarin@gmail.com>
2021-03-11 23:35:27 -0500

    Add phone number and email to CustomUser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUser(AbstractUser):
-    pass
+    phone_number = PhoneNumberField(blank=True)
+    email = models.EmailField()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.username
