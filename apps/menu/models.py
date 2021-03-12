0841c92d0994ac31417bc14c951e2b8236fedb7d
Max Garin <maxgarin@gmail.com>
2021-03-11 23:24:34 -0500

    Add related names and dates for orders

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -7,14 +7,33 @@ class MenuItem(models.Model):
     available = models.BooleanField(default=False)
     # image = models.ImageField()
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class OrderItem(models.Model):
-    item = models.ForeignKey(MenuItem, on_delete=models.SET_NULL, null=True)
+    item = models.ForeignKey(
+        MenuItem, on_delete=models.SET_NULL, null=True, related_name="order_items"
+    )
     order_item_order = models.ForeignKey("Order", on_delete=models.CASCADE, null=True)
     quantity = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return f"{self.item} * {self.quantity} for {self.order_item_order}"
+
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    order_items = models.ManyToManyField(OrderItem)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders"
+    )
+    order_items = models.ManyToManyField(OrderItem, blank=True, related_name="orders")
     is_ordered = models.BooleanField(default=False)
+    order_date = models.DateField(auto_now_add=True)
+    order_modified_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"order # {self.pk} {self.user}"
+
+    def confirm_order(self):
+        self.is_ordered = True
+        self.save(update_fields=["is_ordered"])
