d10c266fb1274dca4571530e6104c3d634ccfe46
Max Garin <maxgarin@gmail.com>
2021-03-13 19:48:23 -0500

    Add menu list view and URLS

---
+++ apps/menu/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import MenuListView
+
+app_name = "menu"
+
+urlpatterns = [path("", MenuListView.as_view(), name="list")]
--- apps/menu/views.py
+++ apps/menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import MenuItem
 
-# Create your views here.
+
+class MenuListView(generic.ListView):
+    model = MenuItem
