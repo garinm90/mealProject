7cb1ddcffffc8ca355396ab9f09fa609eb7fd80e
Max Garin <maxgarin@gmail.com>
2021-03-13 19:48:55 -0500

    Configure menu URLS

--- meal_project/urls.py
+++ meal_project/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("apps.main.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("apps.main.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("menu/", include("apps.menu.urls", namespace="menu")),
+]
